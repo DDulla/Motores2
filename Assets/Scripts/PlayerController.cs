264fd7731d448cd2eb4100543f203832f4c2a2d6
DDulla <yaperdilacabeza@outlook.com>
2025-03-30 15:02:03 -0500

    Now movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,53 +5,53 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private LayerMask groundLayer;
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
     private Vector2 moveInput;
-    private bool isGrounded;
-    private int jumpCount = 0;
+    private int jumpCount = 2;
+    public SpriteRenderer spriteRenderer;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Unity Event para movimiento
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    // Unity Event para salto
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed) // Detectar cuándo se presiona
+        if (context.performed)
         {
-            if (isGrounded || jumpCount < 1)
+            if (jumpCount > 0)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                jumpCount++;
+                jumpCount--;
             }
         }
     }
 
-    private void Update()
+    public void ChangeToRed()
     {
-        // Movimiento horizontal
-        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
-
-        // Validar si está en el suelo usando Raycast
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundLayer);
-
-        if (isGrounded)
-        {
-            jumpCount = 0; // Reiniciar el contador de doble salto
-        }
+        spriteRenderer.color = Color.red;
     }
 
-    private void OnDrawGizmos()
+    public void ChangeToGreen()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * 0.1f);
+        spriteRenderer.color = Color.green;
+    }
+
+    public void ChangeToBlue()
+    {
+        spriteRenderer.color = Color.blue;
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 2;
+        }
     }
 }
