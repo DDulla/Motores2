b0cfca34667783215260b828bbe0ca50e9a6ba35
DDulla <yaperdilacabeza@outlook.com>
2025-03-30 18:20:32 -0500

    Timer On!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private TMPro.TextMeshProUGUI timerText;
     public int maxHealth = 10;
     public int currentHealth;
     private Vector2 moveInput;
     public Rigidbody2D rb;
     public SpriteRenderer spriteRenderer;
     private int jumpCount = 2;
+    private bool canChangeColor = true;
+
+    private float elapsedTime = 0f;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = maxHealth;
+        }
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+
+        elapsedTime += Time.deltaTime;
+        if (timerText != null)
+        {
+            int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+            timerText.text = "Time: " + minutes + " . " + seconds;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -24,19 +49,11 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && jumpCount > 0)
         {
-            if (jumpCount > 0)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                jumpCount--;
-            }
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
         }
-    }
-
-    private void Update()
-    {
-        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -44,6 +61,19 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             jumpCount = 2;
+        }
+
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            canChangeColor = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            canChangeColor = true;
         }
     }
 
@@ -56,20 +86,34 @@ public class PlayerController : MonoBehaviour
             currentHealth = 0;
             Debug.Log("Game Over!");
         }
+
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
+        }
     }
 
     public void ChangeToRed()
     {
-        spriteRenderer.color = Color.red;
+        if (canChangeColor)
+        {
+            spriteRenderer.color = Color.red;
+        }
     }
 
     public void ChangeToGreen()
     {
-        spriteRenderer.color = Color.green;
+        if (canChangeColor)
+        {
+            spriteRenderer.color = Color.green;
+        }
     }
 
     public void ChangeToBlue()
     {
-        spriteRenderer.color = Color.blue;
+        if (canChangeColor)
+        {
+            spriteRenderer.color = Color.blue;
+        }
     }
 }
